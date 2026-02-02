45eac123db527b00001f93d8c079a449d561e9be
Mike B. <pub.mikeb@outlook.com>
2026-02-02 11:53:09 +0100

    Initial commit

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/* eslint-disable no-magic-numbers */
+const config = {
+	bracketSameLine: true,
+	bracketSpacing: false,
+	printWidth: 1000,
+	quoteProps: "consistent",
+	singleAttributePerLine: true,
+	tabWidth: 4,
+	useTabs: true,
+};
+
+export default config;
